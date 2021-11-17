8f0b899217c2155ca96425fa63c0d94da95b84a2
pivolan <pivolan@gmail.com>
2021-11-17 15:29:15 +0200

    edit message

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 
 const TELEGRAM_BOT_API = "2124047747:AAFjAHlV37rFCbjqZ9c6CmqtCREUTYOydQE"
 
+var botMessageId int
+var userMessageId int
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI(TELEGRAM_BOT_API)
 	if err != nil {
@@ -20,6 +23,17 @@ func main() {
 
 	for update := range updates {
 		go func(update tgbotapi.Update) {
+			if update.InlineQuery != nil {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "inline query: "+update.InlineQuery.Query)
+				bot.Send(msg)
+				return
+			}
+			if update.CallbackQuery != nil { //on button clicked
+				msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "calback query: "+update.CallbackQuery.Data)
+				bot.Send(msg)
+				return
+			}
+
 			message := update.Message
 
 			if message.IsCommand() {
@@ -36,7 +50,7 @@ func main() {
 					return
 				case "khide": //hide keyboard
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Скрыл клаву"))
-					msg.ReplyMarkup = DefaultKeyboard()
+					msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
 					bot.Send(msg)
 					return
 				case "buttons": //show buttons inline keyboard
@@ -46,19 +60,39 @@ func main() {
 					bot.Send(msg)
 					return
 				case "markdown": //markdown message smiles
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("*Нажмите* **curs** _italy_\n# header 🙂 some text \xF0\x9F\x98\x81 some text"))
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("*Нажмите* **curs** _italy_\n\n# header\n\n 🙂 some text \xF0\x9F\x98\x81 some text"))
 					msg.ParseMode = tgbotapi.ModeMarkdown
 					bot.Send(msg)
 					return
+				case "edit": //edit bot message
+					msg := tgbotapi.NewEditMessageText(update.Message.Chat.ID, botMessageId, fmt.Sprintf("message changed"))
+					msg.ParseMode = tgbotapi.ModeMarkdown
+					bot.Send(msg)
+					return
+				case "edit_self": //user message cannot be edited
+					msg := tgbotapi.NewEditMessageText(update.Message.Chat.ID, userMessageId, fmt.Sprintf("your message changed"))
+					msg.ParseMode = tgbotapi.ModeMarkdown
+					_, err := bot.Send(msg)
+					if err != nil {
+						fmt.Println(err)
+					}
+					return
 				default:
+					commands := []string{"/k", "/khide", "/buttons", "/markdown", "/edit", "/edit_self"}
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("command getted: %s\n commands allowed:\n%s", update.Message.Command(), strings.Join(commands, "\n")))
+					bot.Send(msg)
 				}
 				return
 			}
-
 			switch message.Text {
 			default:
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Нажмите Next чтобы получить следующую историю"))
-				bot.Send(msg)
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Text: %s, ChatID: %d, username: %s", message.Text, message.Chat.ID, message.Chat.UserName))
+				r, err := bot.Send(msg)
+				if err != nil {
+					return
+				}
+				botMessageId = r.MessageID
+				userMessageId = message.MessageID
 				return
 			}
 		}(update)
